65f2f32ee7d089cba4e481e1ef9696e1a806effb
Ahljennetics <64159706+Ahljenn@users.noreply.github.com>
2022-05-20 00:43:29 +0000

    Search bar added no style

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import '../styles/App.css';
 import Q from '../data/questions.json';
 
 function App() {
-
-  const [qData] = useState(Q);
   
+  const [searchTerm, setSearchTerm] = useState("");
+  const [qData] = useState(Q);
   return (
     <main>
-        {qData.map((data) => (
-          <div class="qa-container">
-            <h4 class="question">{data.question}</h4>
-            <p class="answer">{data.answer}</p>
-          </div>
-        ))}
+      <input
+        type="text"
+        placeholder="Search question"
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      {qData.filter((val) => {
+        if (searchTerm == ""){
+          return val;
+        } else if (val.question.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val;
+        }
+      }).map((val, key) => {
+          return (
+            <div class="qa-container">
+              <h4 class="question">{val.question}</h4>
+              <p class="answer">{val.answer}</p>
+            </div>
+          );
+      })}
     </main>
   );
 }
